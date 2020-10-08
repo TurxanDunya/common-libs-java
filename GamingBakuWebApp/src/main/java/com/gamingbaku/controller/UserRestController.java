371f20398502328c61daece5427358e94b46f26b
TurxanDunya <turxan.dunya@gmail.com>
2020-10-08 14:59:32 +0400

    Last Changes
    Connection problem solved

--- GamingBakuWebApp/src/main/java/com/gamingbaku/controller/UserRestController.java
+++ GamingBakuWebApp/src/main/java/com/gamingbaku/controller/UserRestController.java
@@ -5,7 +5,7 @@ import com.gamingbaku.dto.UserDTO;
 import com.gamingbaku.entity.User;
 import com.gamingbaku.service.inter.UserServiceInter;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,31 +15,30 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@ComponentScan(basePackages = "com.gamingbaku")
 public class UserRestController {
 
     @Autowired
     private UserServiceInter userServiceInter;
 
-//    @GetMapping("/users")
-//    public ResponseEntity<ResponseDTO> getUsers(
-//            @RequestParam(name="name", required = false) String name,
-//            @RequestParam(name="surname", required = false) String surname
-//    ){
-//        List<User> users = userServiceInter.getAll(name, surname);
-//        List<UserDTO> userDTOS = new ArrayList<>();
-//
-//        for (int i = 0; i<users.size(); i++){
-//            User u = users.get(i);
-//            userDTOS.add(new UserDTO(u));
-//        }
-//
-//
-//        return ResponseEntity.ok(ResponseDTO.of(userDTOS));
-//    }
-
     @GetMapping("/users")
-    public ResponseEntity getUsers(){
-        return ResponseEntity.status(HttpStatus.OK).body("yahoo");
+    public ResponseEntity<ResponseDTO> getUsers(
+            @RequestParam(name="name", required = false) String name,
+            @RequestParam(name="surname", required = false) String surname
+    ){
+        List<User> users = userServiceInter.getAll(name, surname);
+        List<UserDTO> userDTOS = new ArrayList<>();
+
+        for (int i = 0; i<users.size(); i++){
+            User u = users.get(i);
+            userDTOS.add(new UserDTO(u));
+        }
+
+        return ResponseEntity.ok(ResponseDTO.of(userDTOS));
     }
 
+    @GetMapping("/login")
+    public String index() {
+        return "Salam";
+    }
 }

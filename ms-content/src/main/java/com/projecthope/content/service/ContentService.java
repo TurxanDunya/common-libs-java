854ec116ce34008f7ef06c95d2e81420b54770a5
Turxan Dunyamaliyev <turxan.dunya97@gmail.com>
2022-04-22 00:10:43 +0400

    Basics of downloading file from minio implemented

--- ms-content/src/main/java/com/projecthope/content/service/ContentService.java
+++ ms-content/src/main/java/com/projecthope/content/service/ContentService.java
@@ -2,14 +2,17 @@ package com.projecthope.content.service;
 
 import com.projecthope.content.config.MinioClientFactory;
 import com.projecthope.content.config.properties.MinioProperties;
+import com.projecthope.content.dto.request.DownloadRequest;
 import com.projecthope.content.dto.request.UploadRequest;
 import com.projecthope.content.dto.response.UploadResponse;
 import com.projecthope.content.model.ContentClientBucket;
+import io.minio.GetObjectArgs;
 import io.minio.GetPresignedObjectUrlArgs;
 import io.minio.MinioClient;
 import io.minio.PutObjectArgs;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ByteArrayResource;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -61,8 +64,27 @@ public class ContentService {
             log.error("Occured error when upload file");
 //            throw ServiceException.of(
 //                    ErrorCodes.FILE_UPLOAD_ERROR, "Error occurred when upload file");
-            throw new RuntimeException("Occured error when upload file");
+            throw new RuntimeException("Occurred error when upload file");
         }
+    }
+
+    public ByteArrayResource download(DownloadRequest request) {
+        GetObjectArgs objectArgs = GetObjectArgs.builder()
+                .bucket(minioProperties.getBucket())
+                .object(request.getPresignedUrl() + request.getFileName())
+                .build();
+
+        ContentClientBucket contentClient = request.getContentClientBucket();
+        MinioClient minioClient = MinioClientFactory.getMinioClient(contentClient);
+
+        byte[] bytes;
+        try {
+            bytes = minioClient.getObject(objectArgs).readAllBytes();
+        } catch (Exception e) {
+            throw new RuntimeException("Occurred error when upload file");
+        }
+
+        return new ByteArrayResource(bytes);
     }
 
 }
